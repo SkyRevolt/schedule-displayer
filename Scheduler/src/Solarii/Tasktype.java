30bf936a755ec75f4da6b8c5438adf90e211f605
fEVER <cl0udydann17@gmail.com>
2021-03-04 14:09:33 +0800

    removed functionality, bad idea

--- Scheduler/src/Solarii/Tasktype.java
+++ Scheduler/src/Solarii/Tasktype.java
@@ -7,22 +7,10 @@ public class Tasktype {
 
     public static void main(String[]args){
 
-    int maxquick = 5;
-    int maxprolonged = 3;
-    int maxvisual = 4;
-    int maxcommon = 5;
+ 
 
-    Random rng = new Random();
-
-    Display.qran = rng.nextInt(maxquick);
-    Display.pran = rng.nextInt(maxprolonged);
-    Display.vran = rng.nextInt(maxvisual);
-    Display.cran = rng.nextInt(maxcommon);
     }
 
-    public static int qran;
-    public static int pran;
-    public static int vran;
-    public static int cran;
+ 
 }
 
